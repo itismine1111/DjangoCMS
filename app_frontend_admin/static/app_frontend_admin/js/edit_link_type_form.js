c338062c6ffae0e46e36dcdcc3a0cf55012b90df
Naman <naman@naman.com>
2023-02-02 19:23:28 +0530

    Feb 2,2023(2)

--- app_frontend_admin/static/app_frontend_admin/js/edit_link_type_form.js
+++ app_frontend_admin/static/app_frontend_admin/js/edit_link_type_form.js
@@ -34,6 +34,7 @@ $(document).ready(function () {
         processData: true,
         success: function (response){
             linkTypeObj = response["data"];
+            document.getElementById("linkTypeInputField").value = linkTypeObj["linkType"];
             console.log(response)
         },
 
@@ -43,58 +44,58 @@ $(document).ready(function () {
         }
     })
 
-    // $("#edit_link_type_form").submit(function (event) {
-    //     event.preventDefault();
-    //     removeErrorParas();
-    //     var form = document.getElementById("add_link_type_form");
-    //     var formData = new FormData(form);
+    $("#edit_link_type_form").submit(function (event) {
+        event.preventDefault();
+        removeErrorParas();
+        var form = document.getElementById("edit_link_type_form");
+        var formData = new FormData(form);
 
-    //     var object = {};
-    //     formData.forEach(function(value, key){
-    //         object[key] = value;
+        var object = {};
+        formData.forEach(function(value, key){
+            object[key] = value;
 
 
-    //     });
+        });
 
-    //     if(!editLinkTypeFormIsValid(object)){
-    //         return;
-    //     }
+        if(!editLinkTypeFormIsValid(object)){
+            return;
+        }
 
-    //     var json = JSON.stringify(object)
-    //     console.warn(json);
+        var json = JSON.stringify(object)
+        console.warn(json);
   
-    //     $.ajax({
-    //         type: "POST",
-    //         url: API_BASE_URL_ADMIN + "linktype/",
-    //         data: json,
-    //         headers: {
-    //             'Content-Type':'application/json'
-    //         },
-    //         processData: false,
-    //         success: function (response){
-    //             console.log(response)
-    //             window.location.replace(LIST_LINK_TYPE_URL_ADMIN);
-    //         },
+        $.ajax({
+            type: "PATCH",
+            url: API_BASE_URL_ADMIN + "linktype/" + linkTypeObjId + "/",
+            data: json,
+            headers: {
+                'Content-Type':'application/json'
+            },
+            processData: false,
+            success: function (response){
+                console.log(response)
+                window.location.replace(LIST_LINK_TYPE_URL_ADMIN);
+            },
 
-    //         error: function(response){
-    //             console.error(response)
-    //             if(response.responseJSON["success"] === false){
-    //                 handleBackendErrors(response.responseJSON["data"]);
-    //             }
-    //             showToast("error", 'Error encountered while creating new Link Type');
-    //         }
-    //     })
-    // });
+            error: function(response){
+                console.error(response)
+                if(response.responseJSON["success"] === false){
+                    handleBackendErrors(response.responseJSON["data"]);
+                }
+                showToast("error", 'Error encountered while creating new Link Type');
+            }
+        })
+    });
 
-    // function editLinkTypeFormIsValid(object){
-    //     isValid = true;
-    //     if(object["linkType"] === "" || object["linkType"] === null){
-    //         showErrorMessage("linkType", ["This field may not be blank.",]);
-    //         isValid = false;
-    //     }
+    function editLinkTypeFormIsValid(object){
+        isValid = true;
+        if(object["linkType"] === "" || object["linkType"] === null){
+            showErrorMessage("linkType", ["This field may not be blank.",]);
+            isValid = false;
+        }
     
-    //     return(isValid);
-    //   }  
+        return(isValid);
+      }  
 
 });
 
